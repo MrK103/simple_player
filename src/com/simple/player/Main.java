7ed4a5f6f8877e5d9734fccff99b18a604585fc1
MrK103 <i.love.rock182@gmail.com>
2021-12-24 12:10:47 +0300

    delete some stuff

--- src/com/simple/player/Main.java
+++ src/com/simple/player/Main.java
@@ -9,10 +9,5 @@ public class Main {
 
     public static void main(String[] args) {
         GuiMP3 guiMP3 = new GuiMP3();
-        try {
-            UIManager.setLookAndFeel(new FlatLightLaf());
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        }
     }
 }
